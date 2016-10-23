086179d125fa5b3e9b30e5bce6e17f370177fc0e
Ankit Zalani <azalani@akamai.com>
2016-10-23 19:57:11 +0530

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name currybhariServiceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the currybhariServiceApp
+ */
+angular.module('currybhariServiceApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
