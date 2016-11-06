e8a7eb2660b3a1ba45895b9a05532640a007cf5a
Ankit Zalani <ankit.littlegenius@gmail.com>
2016-11-06 22:58:18 +0530

    Added public attribute to static website hosting in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 9000;
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/app', 'public'));
 app.get("/", function(req, res){
   res.sendfile("index.html");
 });
