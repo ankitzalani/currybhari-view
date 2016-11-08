afdc65851e76be5790dfe9c6e43b0a2816c32f7c
ankitzalani <ankit.littlegenius@gmail.com>
2016-11-08 20:39:43 +0530

    Changes to server.js for bootstrap

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
-var express = require("express");
+// var express = require("express");
+// var app = express();
+// var port = process.env.PORT || 9000;
+//
+// app.use(express.static(__dirname + '/app'));
+// app.get("/", function(req, res){
+//   res.sendfile("index.html");
+// });
+//
+// app.listen(port, function() {
+//     console.log("Listening on port " + port);
+// });
+//
+//
+
+var http = require('http');
+var express = require('express');
+
+// Requiring express exports a function that creates the application. Call it!
 var app = express();
-var port = process.env.PORT || 9000;
+
+// Set port to listen to
+app.set('port', process.env.PORT || 9000);
 
 app.use(express.static(__dirname + '/app'));
-app.get("/", function(req, res){
-  res.sendfile("index.html");
+
+
+// Tell express to use the router middleware
+// Can be omitted if precedence doesn't matter
+// (e.g. for loading static resources)
+app.use(app.router);
+
+// Add callback handler for home (/) route
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
 });
 
-app.listen(port, function() {
-    console.log("Listening on port " + port);
+// Create http server by passing "app" to it:
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 });
