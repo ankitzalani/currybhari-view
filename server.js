96ca58eb4f2c361c52d733078141816aa7fde625
Ankit Zalani <ankit.littlegenius@gmail.com>
2016-11-12 21:28:19 +0530

    Added api for productDetails/id

--- server.js
+++ server.js
@@ -24,3 +24,13 @@ app.get("/productDetails", function(req, res) {
             });
     });
 });
+
+app.get("/productDetails/:id", function(req, res) {
+    pg.connect(databaseURL, function(err, client) {
+        if (err) throw err;
+        client
+            .query('SELECT * FROM productDetails where productId=' + req.params.id + ';', function(err, result) {
+                res.status(200).json(result.rows);
+            });
+    });
+});
