cab0561b7d89c4511f02133e85797dce6d016a5c
ankitzalani <ankit.littlegenius@gmail.com>
2016-11-12 14:54:13 +0530

    Added logging

--- server.js
+++ server.js
@@ -13,39 +13,16 @@ app.listen(port, function() {
     console.log("Listening on port " + port);
 });
 
+console.log(process.env.DATABASE_URL);
 
-// pg.defaults.ssl = true;
-// instantiate a new client
-// the client will read connection information from
-// the same environment variables used by postgres cli tools
-var client = new pg.Client();
-
-// connect to our database
-client.connect(function (err) {
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
 
-  // execute a query on our database
-  client.query('SELECT * from productdetails', [process.env.DATABASE_URL], function (err, result) {
-    if (err) throw err;
-
-    // just print the result to the console
-    console.log(result.rows[0]); // outputs: { name: 'brianc' }
-
-    // disconnect the client
-    client.end(function (err) {
-      if (err) throw err;
-    });
-  });
+    // client
+    //   .query('SELECT * FROM productdetails;')
+    //   .on('row', function(row) {
+    //     console.log(JSON.stringify(row));
+    //   });
 });
-
-
-// pg.connect(process.env.DATABASE_URL, function(err, client) {
-//   if (err) throw err;
-//   console.log('Connected to postgres! Getting schemas...');
-//
-//     client
-//       .query('SELECT * FROM productdetails;')
-//       .on('row', function(row) {
-//         console.log(JSON.stringify(row));
-//       });
-// });
