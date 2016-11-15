adbf7b0683536644a45df637bd9531849cedc037
Ankit Zalani <ankit.littlegenius@gmail.com>
2016-11-16 00:08:55 +0530

    Removed unnecessary controller

--- client/controller/ProductController.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module("curryBhariApp", []);
-
-app.controller("ProductController", ['$scope','Products', function($scope, Products) {
-    $scope.products = Products.list();
-
-    $scope.categories = Products.categories();
-}]);
