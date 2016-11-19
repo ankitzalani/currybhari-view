c571043050d39948b07160f29ea1d8f1d16e6229
Ankit Zalani <ankit.littlegenius@gmail.com>
2016-11-20 01:01:18 +0530

    changes to delete product from cart.

--- client/cart/cart.controller.js
+++ client/cart/cart.controller.js
@@ -1,12 +1,13 @@
 angular.module('curryBhariApp')
-  .controller("cartController", ['$scope', '$http', '$q','Cart', function(
-  $scope,
-  $http, $q, Cart) {
-    $scope.products = Cart.products;
+    .controller("cartController", ['$scope', '$http', '$q', 'Cart', function(
+        $scope,
+        $http, $q, Cart) {
+        $scope.products = Cart.products;
 
-    $scope.removeItem = function(product) {
-        $scope.products = $scope.products.filter(function(p){
-            return product.productid !== p.productid;
-        });
-    }
-}]);
+        $scope.removeItem = function(product) {
+            Cart.products = Cart.products.filter(function(p) {
+                return product.productid !== p.productid;
+            });
+            $scope.products = Cart.products;
+        }
+    }]);
