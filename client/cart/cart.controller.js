4809ce48090fbaf7b6c45e50382cd625036b18ef
Ankit Zalani <ankit.littlegenius@gmail.com>
2016-11-20 11:16:09 +0530

    Removed $

--- client/cart/cart.controller.js
+++ client/cart/cart.controller.js
@@ -2,12 +2,19 @@ angular.module('curryBhariApp')
     .controller("cartController", ['$scope', '$http', '$q', 'Cart', function(
         $scope,
         $http, $q, Cart) {
-        $scope.products = Cart.products;
 
-        $scope.removeItem = function(product) {
-            Cart.products = Cart.products.filter(function(p) {
-                return product.productid !== p.productid;
-            });
-            $scope.products = Cart.products;
-        }
+        $scope.cart = Cart.cart;
+
+        $scope.removeItem = function(id) {
+            Cart.removeProduct(id);
+            $scope.cart = Cart.cart;
+        };
+
+        $scope.incrementQuantity = function(id) {
+            Cart.incrementQuantity(id);
+        };
+
+        $scope.decrementQuantity = function(id) {
+            Cart.decrementQuantity(id);
+        };
     }]);
--- client/service/Cart.js
+++ client/service/Cart.js
@@ -1,13 +1,40 @@
 'use strict';
 
-var appconfig = {
-    host: 'https://currybhari-view.herokuapp.com'
-};
-
 angular.module('curryBhariApp').service('Cart', ['$http', '$q', function($http, $q) {
-    this.products = [];
+    this.count = 0;
+    this.cart = [];
+    this.grandTotal = 0;
 
     this.addProduct = function(product) {
-        this.products.push(product);
+        this.cart.push({
+            'id': this.count++,
+            'product': product,
+            'quantity': 1,
+            'total': product.rate
+        });
+    };
+
+    this.removeProduct = function(id) {
+        this.cart = this.cart.filter(function(temp) {
+            return id !== temp.id;
+        });
+    };
+
+    this.incrementQuantity = function(id) {
+        for (var i = 0; i < this.cart.length; i++) {
+            if (this.cart[i].id == id) {
+                this.cart[i].quantity++;
+                this.cart[i].total = this.cart[i].quantity * this.cart[i].product.rate;
+            }
+        }
+    };
+
+    this.decrementQuantity = function(id) {
+        for (var i = 0; i < this.cart.length; i++) {
+            if (this.cart[i].id == id) {
+                this.cart[i].quantity--;
+                this.cart[i].total = this.cart[i].quantity * this.cart[i].product.rate;
+            }
+        }
     };
 }]);
