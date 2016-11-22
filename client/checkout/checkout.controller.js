0b6cb5f724b1b963a4b8f8285fc222ef383866d0
Ankit Zalani <ankit.littlegenius@gmail.com>
2016-11-22 19:09:13 +0530

    changes for checkout controller

--- client/checkout/checkout.controller.js
+++ client/checkout/checkout.controller.js
@@ -2,6 +2,8 @@ angular.module('curryBhariApp')
   .controller("checkoutController", ['$scope', '$http', '$q', function(
   $scope,
   $http, $q) {
-
+    $scope.login = function() {
+        $( "#loginDialogue" ).dialog();
+    };
 
 }]);
