9df5978c7eea579cda1337365579213b0484e53e
Ankit Zalani <ankit.littlegenius@gmail.com>
2016-11-26 22:11:40 +0530

    Added logging

--- server/database/index.js
+++ server/database/index.js
@@ -3,7 +3,8 @@
 var mongoose = require('mongoose');
 var config = require('../config');
 
-mongoose.connect(config.MONGO_URI);
+console.log('MONGO_URI: ' + config.MONGO_URI);
+mongoose.connect(config.MONGO_URI, {server: { auto_reconnect: true }});
 mongoose.connection.on('error', function(err) {
     console.error('MongoDB connection error: ' + err);
 });
