5d59e6010589778c7f95fd9ffefd71f56f6c3e6e
PANKAJKHANDKAR <pankajkhandkar@gmail.com>
2016-11-26 15:05:51 +0530

    Changes done

--- client/service/Products.js
+++ client/service/Products.js
@@ -1,17 +1,27 @@
 'use strict';
 
 var appconfig = {
-    host: 'https://currybhari-view.herokuapp.com'
+  host: 'https://currybhari-view.herokuapp.com'
 };
 
-angular.module('curryBhariApp').service('Products', ['$http', '$q', function($http, $q) {
-    this.list = function() {
+angular.module('curryBhariApp').service('Products', ['$http', '$q', function(
+  $http, $q) {
+  this.list = function() {
 
-        var promise = $http.get(appconfig.host + '/productDetails').success(
-            function(data) {
-                return data;
-            });
+    var promise = $http.get(appconfig.host + '/productDetails').success(
+      function(data) {
+        return data;
+      });
 
-        return promise;
-    };
+    return promise;
+  };
+
+  this.getProduct = function(id) {
+    var promise = $http.get('http://localhost:9000/product?id=1').success(
+      function(data) {
+        return data;
+      });
+
+    return promise;
+  }
 }]);
--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ var databaseURL = process.env.DATABASE_URL;
 var mockObj = require('./debug/fetch.js');
 
 var corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200
+  origin: '*',
+  optionsSuccessStatus: 200
 };
 
 pg.defaults.ssl = true;
@@ -15,37 +15,52 @@ var debug = true;
 
 
 module.exports = function(app) {
-    app.get('/productDetails', cors(corsOptions), function(req, res) {
+  app.get('/productDetails', cors(corsOptions), function(req, res) {
 
-        if (debug == true) {
-            mockObj.fetchMock('productDetails', function(data) {
-                res.send(data);
-            });
-        } else {
-            pg.connect(databaseURL, function(err, client) {
-                if (err) throw err;
-                client
-                    .query('SELECT * FROM productDetails;', function(err, result) {
-                        res.send(result.rows);
-                    });
-            });
-        }
-    });
+    if (debug == true) {
+      mockObj.fetchMock('productDetails', function(data) {
+        res.send(data);
+      });
+    } else {
+      pg.connect(databaseURL, function(err, client) {
+        if (err) throw err;
+        client
+          .query('SELECT * FROM productDetails;', function(err,
+            result) {
+            res.send(result.rows);
+          });
+      });
+    }
+  });
 
-    app.get('/config', cors(corsOptions), function(req, res) {
-        console.log('config');
-        if (debug == true) {
-            mockObj.fetchMock('config', function(data) {
-                res.send(data);
-            });
-        } else {
-            pg.connect(databaseURL, function(err, client) {
-                if (err) throw err;
-                client
-                    .query('SELECT * FROM CONFIG;', function(err, result) {
-                        res.send(result.rows);
-                    });
-            });
-        }
+  app.get('/product', cors(corsOptions), function(req, res) {
+    res.send({
+      "productid": "1",
+      "name": "Methi Vada",
+      "description": "Best Methi Vada",
+      "rate": "100",
+      "image": "./images/home/1.jpg",
+      "images": ["./images/product-details/1.jpg",
+        "./images/product-details/similar1.jpg",
+        "./images/product-details/similar2.jpg"
+      ]
     });
+  });
+
+  app.get('/config', cors(corsOptions), function(req, res) {
+    console.log('config');
+    if (debug == true) {
+      mockObj.fetchMock('config', function(data) {
+        res.send(data);
+      });
+    } else {
+      pg.connect(databaseURL, function(err, client) {
+        if (err) throw err;
+        client
+          .query('SELECT * FROM CONFIG;', function(err, result) {
+            res.send(result.rows);
+          });
+      });
+    }
+  });
 };
