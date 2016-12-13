e01349eb0850a90db4f6d8f1fb87a16c42d2b82d
PANKAJKHANDKAR <pankajkhandkar@gmail.com>
2016-12-13 19:37:34 +0530

    No change

--- client/login/login.controller.js
+++ client/login/login.controller.js
@@ -1,50 +1,54 @@
 angular.module('curryBhariApp')
-    .controller("loginController", ['$scope', '$http', '$q', 'UserService', '$state', function(
-        $scope,
-        $http, $q, UserService, $state) {
+  .controller("loginController", ['$scope', '$http', '$q', 'UserService',
+    '$state',
+    function(
+      $scope,
+      $http, $q, UserService, $state) {
 
-        $scope.error = "";
-        $scope.user = {
-            username: "",
-            email: "",
-            password: "",
-            mobile: ""
-        };
+      $scope.error = "";
+      $scope.user = {
+        username: "",
+        email: "",
+        password: "",
+        mobile: ""
+      };
 
-        $scope.login = {
-            username: "",
-            password: ""
-        }
+      $scope.login = {
+        username: "",
+        password: ""
+      }
 
-        $scope.login = function() {
-            if ($scope.login.username.trim().length > 0 &&
-                $scope.login.password.trim().length > 0) {
-                UserService.login($scope.login).then(function(promise) {
-                    if (promise.data.error && promise.data.error.length) {
-                        $scope.error = promise.data.error;
-                    } else {
-                        $state.go('checkout');
-                    }
-                });
+      $scope.login = function() {
+        if ($scope.login.username.trim().length > 0 &&
+          $scope.login.password.trim().length > 0) {
+          UserService.login($scope.login).then(function(promise) {
+            if (promise.data.error && promise.data.error.length) {
+              $scope.error = promise.data.error;
             } else {
-                $scope.error = "Invalid credentials";
-            }
-        }
 
-        $scope.register = function() {
-            if ($scope.user.username.trim().length > 0 &&
-                $scope.user.password.trim().length > 0 &&
-                $scope.user.email.trim().length > 0 &&
-                $scope.user.email.trim().length > 0) {
-                UserService.register($scope.user).then(function(promise) {
-                    if (promise.data.error && promise.data.error.length) {
-                        $scope.error = promise.data.error;
-                    } else {
-                        $state.go("checkout");
-                    }
-                });
-            } else {
-                $scope.error = "Please enter all values to login.";
+              $state.go('checkout');
             }
+          });
+        } else {
+          $scope.error = "Invalid credentials";
         }
-    }]);
+      }
+
+      $scope.register = function() {
+        if ($scope.user.username.trim().length > 0 &&
+          $scope.user.password.trim().length > 0 &&
+          $scope.user.email.trim().length > 0 &&
+          $scope.user.email.trim().length > 0) {
+          UserService.register($scope.user).then(function(promise) {
+            if (promise.data.error && promise.data.error.length) {
+              $scope.error = promise.data.error;
+            } else {
+              $state.go("checkout");
+            }
+          });
+        } else {
+          $scope.error = "Please enter all values to login.";
+        }
+      }
+    }
+  ]);
