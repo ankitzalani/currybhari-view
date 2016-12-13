3726fb7f95bd8f08341a05ece4d3a1e8b90b9917
PANKAJKHANDKAR <pankajkhandkar@gmail.com>
2016-12-13 22:43:39 +0530

    reverting changes

--- client/product-details/product-details.controller.js
+++ client/product-details/product-details.controller.js
@@ -1,29 +1,32 @@
 angular.module('curryBhariApp')
-    .controller("productDetailsController", ['$stateParams', '$scope', '$http', '$q', 'Products', 'Cart', 'Notification', '$cookieStore',
-        function($stateParams,
-            $scope,
-            $http, $q, Products, Cart, Notification, $cookieStore) {
-            $scope.product = $cookieStore.get('product') || [];
-            $scope.id = $stateParams.id;
-            $scope.quantity = 1;
+  .controller("productDetailsController", ['$stateParams', '$scope', '$http',
+    '$q', 'Products', 'Cart', 'Notification', '$cookieStore',
+    function($stateParams,
+      $scope,
+      $http, $q, Products, Cart, Notification, $cookieStore) {
+      $scope.product = $cookieStore.get('product') || [];
+      $scope.id = $stateParams.id;
+      $scope.quantity = 1;
 
-            var getProduct = function(id) {
-                if(id) {
-                  Products.getProduct(id).then(function(promise) {
-                      $scope.product = promise.data;
-                      $cookieStore.put('product', $scope.product);
-                  });
-                }
-            };
-
-            $scope.addToCart = function(product) {
-                Cart.addProduct(product, parseInt($scope.quantity));
-                Notification.success({
-                    message: product.name + ' added to cart',
-                    delay: 1000, positionY: 'bottom', positionX: 'right'
-                });
-            };
-
-            getProduct($scope.id);
+      var getProduct = function(id) {
+        if (id) {
+          Products.getProduct(id).then(function(promise) {
+            $scope.product = promise.data;
+            $cookieStore.put('product', $scope.product);
+          });
         }
-    ]);
+      };
+
+      $scope.addToCart = function(product) {
+        Cart.addProduct(product, parseInt($scope.quantity));
+        Notification.success({
+          message: product.name + ' added to cart',
+          delay: 1000,
+          positionY: 'bottom',
+          positionX: 'right'
+        });
+      };
+
+      getProduct($scope.id);
+    }
+  ]);
