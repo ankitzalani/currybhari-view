058289d979d1644148427af333eb21a3e0df7547
PANKAJKHANDKAR <pankajkhandkar@gmail.com>
2016-12-13 22:06:38 +0530

    reverting changes

--- client/login/login.controller.js
+++ client/login/login.controller.js
@@ -1,61 +1,102 @@
-angular.module('curryBhariApp')
-    .controller("loginController", ['$scope', '$rootScope', '$http', '$q', 'UserService', '$state', function(
-        $scope, $rootScope,
-        $http, $q, UserService, $state) {
+angular.module('curryBhariApp') << << << < HEAD
+  .controller("loginController", ['$scope', '$http', '$q', 'UserService',
+      '$state',
+      function(
+        $scope,
+        $http, $q, UserService, $state) { === === =
+        .controller("loginController", ['$scope', '$rootScope', '$http', '$q',
+              'UserService', '$state',
+              function(
+                $scope, $rootScope,
+                $http, $q, UserService, $state) { >>> >>> > upstream / master
 
-        $scope.error = "";
-        $scope.user = {
-            username: "",
-            email: "",
-            password: "",
-            mobile: ""
-        };
+                $scope.error = "";
+                $scope.user = {
+                  username: "",
+                  email: "",
+                  password: "",
+                  mobile: ""
+                };
 
-        $scope.login = {
-            username: "",
-            password: ""
-        }
+                $scope.login = {
+                  username: "",
+                  password: ""
+                }
+                $scope.login = function() {
+                    if ($scope.login.username.trim().length > 0 &&
+                      $scope.login.password.trim().length > 0) {
+                      UserService.login($scope.login).then(function(promise) {
+                          if (promise.data.error && promise.data.error.length) {
+                            $scope.error = promise.data.error;
+                            $scope.login = function() {
+                              if ($scope.login.username.trim().length > 0 &&
+                                $scope.login.password.trim().length > 0) {
+                                UserService.login($scope.login).then(
+                                  function(promise) {
+                                    if (promise.data.error && promise.data
+                                      .error.length) {
+                                      $scope.error = promise.data.error;
+                                    } else {
+                                      $scope.stateTransition();
+                                    }
+                                  });
+                              } else {
 
-        $scope.login = function() {
-            if ($scope.login.username.trim().length > 0 &&
-                $scope.login.password.trim().length > 0) {
-                UserService.login($scope.login).then(function(promise) {
-                    if (promise.data.error && promise.data.error.length) {
-                        $scope.error = promise.data.error;
-                    } else {
-                        $scope.stateTransition();
-                    }
-                });
-            } else {
-                $scope.error = "Invalid credentials";
-            }
-        }
+                                $state.go('checkout');
+                              }
+                            });
+                        } else {
+                          $scope.error = "Invalid credentials";
+                        }
+                      }
 
-        $scope.stateTransition = function() {
-            if ($rootScope.previousState.name === 'cart') {
-                $state.go('checkout');
-            } else if ($rootScope.previousState.name === '') {
-                $state.go('container');
-            } else {
-                $state.go($rootScope.previousState.name);
-            }
-        }
+                      << << << < HEAD
+                      $scope.register = function() {
+                        if ($scope.user.username.trim().length > 0 &&
+                          $scope.user.password.trim().length > 0 &&
+                          $scope.user.email.trim().length > 0 &&
+                          $scope.user.email.trim().length > 0) {
+                          UserService.register($scope.user).then(function(
+                              promise) {
+                              if (promise.data.error && promise.data.error.length) {
+                                $scope.error = promise.data.error; === === =
+                                $scope.stateTransition = function() {
+                                  if ($rootScope.previousState.name ===
+                                    'cart') {
+                                    $state.go('checkout');
+                                  } else if ($rootScope.previousState.name ===
+                                    '') {
+                                    $state.go('container');
+                                  } else {
+                                    $state.go($rootScope.previousState.name);
+                                  }
+                                }
 
-        $scope.register = function() {
-            if ($scope.user.username.trim().length > 0 &&
-                $scope.user.password.trim().length > 0 &&
-                $scope.user.email.trim().length > 0 &&
-                $scope.user.mobile.trim().length > 0) {
-                  
-                UserService.register($scope.user).then(function(promise) {
-                    if (promise.data.error && promise.data.error.length) {
-                        $scope.error = promise.data.error;
-                    } else {
-                        $scope.stateTransition();
-                    }
-                });
-            } else {
-                $scope.error = "Please enter all values to login.";
-            }
-        }
-    }]);
+                                $scope.register = function() {
+                                  if ($scope.user.username.trim().length >
+                                    0 &&
+                                    $scope.user.password.trim().length >
+                                    0 &&
+                                    $scope.user.email.trim().length > 0 &&
+                                    $scope.user.mobile.trim().length > 0) {
+
+                                    UserService.register($scope.user).then(
+                                      function(promise) {
+                                        if (promise.data.error &&
+                                          promise.data.error.length) {
+                                          $scope.error = promise.data.error;
+                                        } else {
+                                          $scope.stateTransition();
+                                        }
+                                      }); >>> >>> > upstream / master
+                                  } else {
+                                    $state.go("checkout");
+                                  }
+                                });
+                            } else {
+                              $scope.error =
+                                "Please enter all values to login.";
+                            }
+                          }
+                        }
+                      ]);
