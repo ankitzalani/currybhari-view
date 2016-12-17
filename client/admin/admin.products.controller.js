478c7f8abcd41d85063be84c059eb4352a3298a1
Ankit Zalani <azalani@akamai.com>
2016-12-17 17:55:37 +0530

    Changes for managing products

--- client/admin/admin.products.controller.js
+++ client/admin/admin.products.controller.js
@@ -1,6 +1,10 @@
-angular.module('curryBhariApp').controller("adminProductsController", ['$scope', '$http', '$q', '$state', 'Products', function($scope, $http, $q, $state, Products) {
+angular.module('curryBhariApp').controller("adminProductsController", ['$scope', '$http', '$q', '$state', 'Products', 'Upload', 'Notification', function($scope, $http, $q, $state, Products, Upload, Notification) {
 
-    $scope.products = [];
+    $scope.products = {
+        data:[]
+    }
+
+    $scope.file = {};
 
     $scope.productObj = {
         name: '',
@@ -13,7 +17,7 @@ angular.module('curryBhariApp').controller("adminProductsController", ['$scope',
     $scope.getProducts = function() {
         var controller = this;
         Products.list(true).then(function(promise) {
-            controller.products = promise.data;
+            controller.products.data = promise.data;
         });
     };
 
@@ -38,11 +42,48 @@ angular.module('curryBhariApp').controller("adminProductsController", ['$scope',
     $scope.delete = function(product) {
         var controller = this;
         Products.delete(product._id).then(function(data) {
-            controller.products = controller.products.filter(function(obj) {
-                return product._id !== obj._id;
+            Notification.success({
+                message: product.name + ' deleted',
+                delay: 1000,
+                positionY: 'bottom',
+                positionX: 'right'
             });
+            controller.getProducts();
         });
     }
+
+    $scope.submit = function() { //function to call on form submit
+        var controller = this;
+        $scope.upload(controller.file); //call upload function
+    }
+
+    $scope.upload = function(file) {
+        var controller = this;
+        Upload.upload({
+            url: '/product/image/upload', //webAPI exposed to upload the file
+            data: {
+                file: file
+            } //pass file as data, should be user ng-model
+        }).then(function(resp) { //upload function returns a promise
+            if (resp.data.error_code === 0) { //validate success
+                $state.go('adminProducts');
+
+                controller.productObj.image = "./images/uploads/" + resp.data.file_name;
+                Products.add($scope.productObj).then(function(data) {
+                    $state.go('adminProducts');
+                });
+            } else {
+                //error
+            }
+        }, function(resp) { //catch error
+            console.log('Error status: ' + resp.status);
+        }, function(evt) {
+            console.log(evt);
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+            controller.progress = 'progress: ' + progressPercentage + '% '; // capture upload progress
+        });
+    };
 
     $scope.getProducts();
 }]);
--- server/apis/productDao.js
+++ server/apis/productDao.js
@@ -3,6 +3,26 @@ var product = require('../database/modals/Product');
 var mock = require('../debug/fetch');
 var utils = require('./utils');
 var loadFromFile = false;
+var message = require('../messages');
+var multer = require('multer');
+
+var filenameStr = '';
+
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function(req, file, cb) {
+        cb(null, './client/images/uploads');
+    },
+    filename: function(req, file, cb) {
+        var datetimestamp = Date.now();
+        filenameStr = file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1];
+        cb(null, filenameStr);
+    }
+});
+
+
+var upload = multer({ //multer settings
+    storage: storage
+}).single('file');
 
 module.exports = function(app) {
     app.get('/products', cors(utils.corsOptions), function(req, res) {
@@ -44,7 +64,25 @@ module.exports = function(app) {
 
         prod.save(function(error, prod) {
             if (error) return utils.throwError(response, message.SOMETHING_WENT_WRONG);
-            return utils.throwSuccess(response, createUserObjResponse(userObj));
+            return utils.throwSuccess(response, {});
+        });
+    });
+
+    /** API path that will upload the files */
+    app.post('/product/image/upload', function(req, res) {
+        filenameStr = '';
+        upload(req, res, function(err) {
+            if (err) {
+                return res.json({
+                    error_code: 1,
+                    err_desc: err
+                });
+            }
+            return res.json({
+                error_code: 0,
+                err_desc: null,
+                file_name: filenameStr
+            });
         });
     });
 
