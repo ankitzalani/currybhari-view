af5b10c7f6b68464af68d761c4fcaac2d902af89
Ankit Zalani <ankit.littlegenius@gmail.com>
2019-04-09 00:35:46 +0530

    Update index.js

--- server/database/index.js
+++ server/database/index.js
@@ -3,7 +3,7 @@
 var mongoose = require('mongoose');
 var config = require('../config');
 
-mongoose.connect(config.MONGO_URI, {server: { auto_reconnect: true }});
+mongoose.connect(config.MONGO_URI, {server: { auto_reconnect: true,  useMongoClient: true }});
 mongoose.Promise = global.Promise;
 mongoose.connection.on('error', function(err) {
     console.error('MongoDB connection error: ' + err);
